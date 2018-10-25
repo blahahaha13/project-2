5ad5d29f680d0d6836a691e6c04e9292584442e2
blahahaha13 <gmony1558@gmail.com>
2018-10-25 13:55:46 -0700

    server stuff

--- motorcycle/models.py
+++ motorcycle/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Reciept(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  email = models.CharField(max_length=100)
+  amount = models.IntegerField()
+  description = models.TextField()
+
