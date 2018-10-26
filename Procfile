654f468dd9756074ab852fe39caccf988dadb836
blahahaha13 <gmony1558@gmail.com>
2018-10-26 16:52:40 -0700

    preped for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn contacts_project.wsgi
--- beemer_boys/settings.py
+++ beemer_boys/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,7 +128,7 @@ STATIC_URL = '/static/'
 # STRIPE_PUBLISHABLE_KEY = pk_test_oWcgMJxoTjtnsREeFyHNiuOd
 # Secret
 
-
+django_heroku.settings(locals())
 
 
 
