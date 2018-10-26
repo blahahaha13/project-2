dc5b778c86eac2eed87500110cb654f965ba7481
praveenkhanal <praveenkhanal@Praveens-MacBook-Pro.local>
2018-10-26 14:49:33 -0700

    Api

--- beemer_boys/settings.py
+++ beemer_boys/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'motorcycle',
+    'stripe',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Stripe Setting
+#test keys
+# STRIPE_PUBLISHABLE_KEY = pk_test_oWcgMJxoTjtnsREeFyHNiuOd
+# Secret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- motorcycle/urls.py
+++ motorcycle/urls.py
@@ -5,9 +5,13 @@ urlpatterns = [
   path('', views.landing, name='landing'),
   path('about/', views.about, name='about'),
   path('models/', views.models, name='models'),
-  path('models/<int:model_id>/', views.modelInfo, name='model info'),
+  path('models/<int:model_id>/', views.modelinfo, name='model info'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
   path('register', views.register, name='register'),
+  path('test-stripe', views.test_stripe, name='test'),
+  path('checkout', views.checkout, name='checkout'),
+  path('stripe_default_form', views.stripe_default_form, name='stripe_default_form'),
+
   # path('checkout/', views.checkout, name='checkout'),
 ]
