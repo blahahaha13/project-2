2f5c9fa7d9d614c1dce2c4e93528dbbfb04e3889
blahahaha13 <gmony1558@gmail.com>
2018-10-27 12:51:58 -0700

    merge

--- motorcycle/views.py
+++ motorcycle/views.py
@@ -75,25 +75,26 @@ def register(request):
     return render(request, 'register.html')
 
 class Motorcycle:
-  def __init__(self, name, description, price, img):
+  def __init__(self, name, description, price, img, number):
     self.name = name
     self.description = description
     self.price = price
     self.img = img
+    self.number = number
 
 roadsters = [
-  Motorcycle('R 1200 R', "The BMW G 310 R is the essence of riding pleasure. It was built for pulsing cities and those who simply love riding motorcycles. It is maneuverable, easy to handle and sporty at the same time. Whether you're a tall or small rider, you’ll immediately feel at home on the BMW G 310 R. The bike is lightweight, yet a real powerhouse with its 313 cc engine. Get to work, the next hot spot or out of the city safely and reliably on the G 310 R. Premium quality, excellent workmanship and extraordinary technology ensure intense riding pleasure.", '$14,345', '../static/images/R_1200_R.jpg'),
-  Motorcycle('S 1000 R', "Riding dynamics coupled with touring suitability - at the very highest level: that's the R 1200 RS. With its potent engine and stable suspension, the sports touring bike offers more than just a huge amount of riding pleasure. Thanks to its relaxed, sporty seating position and its perfect wind and weather protection, the bike offers an incredible ride feel when traveling fast and riding along country roads in sporty style.", '$13,995', '../static/images/S_1000_R.jpg'),
-  Motorcycle('F 800 R', "The R nineT Racer lets you relive the era of legendary superbikes. Far removed from obsessive retro romanticism, but rather on a customizable bike with innovative technology and in customary BMW Motorrad quality. Crouched behind the striking half fairing, both hands tight on the low-slung handlebar grips, you can feel the powerful boxer work, you hear its unmistakable roar. And you already know: only a strong character can hold the racing line. On the road, as in life.", '$9,995', '../static/images/F_800_R.jpg'),
-  Motorcycle('G 310 R', 'lorem', '$4,750', '../static/images/G_310_R.jpg')
+  Motorcycle('R 1200 R', "The BMW G 310 R is the essence of riding pleasure. It was built for pulsing cities and those who simply love riding motorcycles. It is maneuverable, easy to handle and sporty at the same time. Whether you're a tall or small rider, you’ll immediately feel at home on the BMW G 310 R. The bike is lightweight, yet a real powerhouse with its 313 cc engine. Get to work, the next hot spot or out of the city safely and reliably on the G 310 R. Premium quality, excellent workmanship and extraordinary technology ensure intense riding pleasure.", '$14,345', '../static/images/R_1200_R.jpg', 1),
+  Motorcycle('S 1000 R', "Riding dynamics coupled with touring suitability - at the very highest level: that's the R 1200 RS. With its potent engine and stable suspension, the sports touring bike offers more than just a huge amount of riding pleasure. Thanks to its relaxed, sporty seating position and its perfect wind and weather protection, the bike offers an incredible ride feel when traveling fast and riding along country roads in sporty style.", '$13,995', '../static/images/S_1000_R.jpg', 2),
+  Motorcycle('F 800 R', "The R nineT Racer lets you relive the era of legendary superbikes. Far removed from obsessive retro romanticism, but rather on a customizable bike with innovative technology and in customary BMW Motorrad quality. Crouched behind the striking half fairing, both hands tight on the low-slung handlebar grips, you can feel the powerful boxer work, you hear its unmistakable roar. And you already know: only a strong character can hold the racing line. On the road, as in life.", '$9,995', '../static/images/F_800_R.jpg', 3),
+  Motorcycle('G 310 R', 'lorem', '$4,750', '../static/images/G_310_R.jpg', 4)
 ]
 
 heritages = [
-  Motorcycle('R nineT', 'lorem', '$15,495', '../static/images/R_nineT.jpg'),
-  Motorcycle('R nineT Pure', 'lorem', '$11,995', '../static/images/R_nineT_Pure.jpg'),
-  Motorcycle('R nineT Racer', 'lorem', '$13,545', '../static/images/R_nineT_Racer.jpg'),
-  Motorcycle('R nineT Scrambler', 'lorem', '$12,995', '../static/images/R_nineT_Scrambler.jpg'),
-  Motorcycle('R ninT Urban G/S', 'lorem', '$12,995', '../static/images/R_nineT_Urban_G:s.jpg')
+  Motorcycle('R nineT', 'lorem', '$15,495', '../static/images/R_nineT.jpg', 5),
+  Motorcycle('R nineT Pure', 'lorem', '$11,995', '../static/images/R_nineT_Pure.jpg', 6),
+  Motorcycle('R nineT Racer', 'lorem', '$13,545', '../static/images/R_nineT_Racer.jpg', 7),
+  Motorcycle('R nineT Scrambler', 'lorem', '$12,995', '../static/images/R_nineT_Scrambler.jpg', 8),
+  Motorcycle('R ninT Urban G/S', 'lorem', '$12,995', '../static/images/R_nineT_Urban_G:s.jpg', 9)
 ]
 
 
