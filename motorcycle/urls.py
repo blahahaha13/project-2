276018edd08547392f69ec18b730e2507e615fb9
blahahaha13 <gmony1558@gmail.com>
2018-10-29 14:07:46 -0700

    updates id name in modelinfo url

--- motorcycle/urls.py
+++ motorcycle/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
   path('', views.landing, name='landing'),
   path('about/', views.about, name='about'),
   path('models/', views.models, name='models'),
-  path('models/<int:model_id>/', views.modelinfo, name='model info'),
+  path('models/<int:motorcycle_id>', views.modelinfo, name='model info'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
   path('register', views.register, name='register'),
@@ -13,4 +13,4 @@ urlpatterns = [
   path('payment', views.payment, name='payment'),
   path('charge/', views.charge, name='charge'),
   path('', views.HomePageView.as_view(), name='home'),
-  ]
+]
